88bb69599455c69c92c8eec8460b7a393215d603
Andre L Gomes <andre@andrelgomes.com>
2024-09-29 13:15:13 -0700

    chore: basic house threejs practice

--- src/components/ui/SystemGraphic/Box.tsx
+++ src/components/ui/SystemGraphic/Box.tsx
@@ -1,24 +1,63 @@
-import React, { useRef, useState } from 'react';
-
-import { Canvas, ThreeElements, useFrame } from '@react-three/fiber';
+import { Addition, Base, Geometry, Subtraction } from '@react-three/csg';
+import { ThreeElements } from '@react-three/fiber';
 import * as THREE from 'three';
 
+const box = new THREE.BoxGeometry();
+const cyl = new THREE.CylinderGeometry(1, 1, 2, 20);
+const tri = new THREE.CylinderGeometry(1, 1, 2, 3);
+
 export default function Box(props: ThreeElements['mesh']) {
-  const meshRef = useRef<THREE.Mesh>(null!);
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-  useFrame((state, delta) => (meshRef.current.rotation.x += delta));
   return (
     <mesh
       {...props}
-      ref={meshRef}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
+      receiveShadow
+      castShadow
     >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : '#2f74c0'} />
+      <Geometry>
+        <Base
+          name='base'
+          geometry={box}
+          scale={[3, 3, 3]}
+        />
+        <Subtraction
+          name='cavity'
+          geometry={box}
+          scale={[2.7, 2.7, 2.7]}
+        />
+        <Addition
+          name='roof'
+          geometry={tri}
+          scale={[2.5, 1.5, 1.425]}
+          rotation={[-Math.PI / 2, 0, 0]}
+          position={[0, 2.2, 0]}
+        />
+        <Door
+          rotation={[0, Math.PI / 2, 0]}
+          position={[-1.425, -0.45, 0]}
+          scale={[1, 0.9, 1]}
+        />
+      </Geometry>
+      <meshStandardMaterial
+        envMapIntensity={0.25}
+        color={'#2f74c0'}
+      />
     </mesh>
   );
 }
+
+const Door = (props) => (
+  <Subtraction {...props}>
+    <Geometry>
+      <Base
+        geometry={box}
+        scale={[1, 2, 1]}
+      />
+      <Addition
+        geometry={cyl}
+        scale={0.5}
+        rotation={[Math.PI / 2, 0, 0]}
+        position={[0, 1, 0]}
+      />
+    </Geometry>
+  </Subtraction>
+);
--- src/components/ui/SystemGraphic/index.tsx
+++ src/components/ui/SystemGraphic/index.tsx
@@ -3,30 +3,20 @@
 import { Canvas } from '@react-three/fiber';
 
 import Box from './Box';
+import Environment from './Environment';
 
 export default function SystemGraphic() {
   return (
     <div className='h-full w-full'>
-      <div className='h-full w-full px-10 pb-5 rounded-xl bg-gray-400'>
-        <div className='flex flex-row space-x-3'>
-          <Canvas>
-            <ambientLight intensity={Math.PI / 2} />
-            <spotLight
-              position={[10, 10, 10]}
-              angle={0.15}
-              penumbra={1}
-              decay={0}
-              intensity={Math.PI}
-            />
-            <pointLight
-              position={[-10, -10, -10]}
-              decay={0}
-              intensity={Math.PI}
-            />
-            <Box position={[-1.2, 0, 0]} />
-            <Box position={[1.2, 0, 0]} />
-          </Canvas>
-        </div>
+      <div className='h-full w-full px-10 pb-5 rounded-xl bg-gray-100'>
+        <Canvas
+          shadows
+          camera={{ position: [-15, 10, 15], fov: 25 }}
+        >
+          <color attach="background" args={['green']}/>
+          <Box />
+          <Environment />
+        </Canvas>
       </div>
     </div>
   );
